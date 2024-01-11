51b98f13cbc6c4e8c9e1921d272a45646b6f862d
1Mashilo <christophermashilo@gmail.com>
2024-01-11 20:52:50 +0200

    modified the file

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-APP_PORT=${PORT:-8000}
-
+SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"christophermashilo@gmail.com"}
 cd /app/
 
-/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm django_k8s.wsgi:application \
-  --bind "0.0.0.0:${APP_PORT}"
+# Activate the virtual environment
+source /app/venv/bin/activate
+
+# Install dependencies (if needed)
+# /app/venv/bin/pip install -r requirements.txt
+
+# Run migrations
+/app/venv/bin/python manage.py migrate --noinput
+
+# Create superuser
+/app/venv/bin/python manage.py createsuperuser --email "$SUPERUSER_EMAIL" --noinput || true
+
+# Start your application using Gunicorn
+/app/venv/bin/gunicorn --worker-tmp-dir /dev/shm django_k8s.wsgi:application --bind "0.0.0.0:8000"
