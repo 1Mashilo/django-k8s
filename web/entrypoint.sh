4ee46cd8e28949aea99224c28bd49e68317e0db3
1Mashilo <christophermashilo@gmail.com>
2024-01-12 15:13:26 +0200

    Removed redundant setup steps from migrate.sh

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"christophermashilo@gmail.com"}
-cd /app/
-
-# Activate the virtual environment
+# Activate virtual environment
 source /app/venv/bin/activate
 
 # Install dependencies (if needed)
-# /app/venv/bin/pip install -r requirements.txt
-
-# Run migrations
-/app/venv/bin/python manage.py migrate --noinput
-
-# Create superuser
-/app/venv/bin/python manage.py createsuperuser --email "$SUPERUSER_EMAIL" --noinput || true
+ /app/venv/bin/pip install -r requirements.txt
 
 # Start your application using Gunicorn
 /app/venv/bin/gunicorn --worker-tmp-dir /dev/shm django_k8s.wsgi:application --bind "0.0.0.0:8000"
